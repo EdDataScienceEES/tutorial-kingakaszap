836087a614fb31340e296a2c0db0132338585f0c
kingakaszap <s2024589@ed.ac.uk>
2022-11-24 16:05:30 +0000

    manipulating dataset and other fun stuff

--- scripts/script.R
+++ scripts/script.R
@@ -14,25 +14,29 @@ names(parks) #shows the names of the columns
 #Kinga Kaszap
 #23/11/2022
 
-#Describing communities
+#Describing communities - or any other informative title
 #Your Name
 #Date
 #Any other comments you want to add
 
+write.csv(parks2, file="datasets/field_data_2")
+parks<- read.csv("datasets/field_data_2")
 #Libraries ----
 library(tidyverse)
 library(ggthemes)
 
 #part 2 - data wrangling ----
 View(parks)
-parks_tidy<- parks %>% gather(species, abundance, c(2:21)) %>% 
-  #organizing into long format, where individual counts(value) is gathered by species (key)
-  #removing NA-s
-  mutate(abundance = parse_number(abundance)) %>% 
-  na.omit()
-  #turning values in the abundance column into numeric - removing notes like "???" and "maybe more"
 
-View(parks_tidy)
+parks_tidy<- parks %>% gather(species, abundance, c(2:21)) %>% 
+  #organizing into long format, where counts of individuals (value) are assigned to species (key)
+  mutate(abundance = parse_number(abundance)) %>% 
+  #removing non-numeric parts (notes) from values in the abundance column
+  na.omit()
+  #removing NA-s
+
+
+View (parks_tidy)
 glimpse(parks_tidy)
 
 
@@ -42,11 +46,33 @@ glimpse(parks_tidy)
 
 (richness<- parks_tidy%>% 
   group_by(site) %>% 
-  summarise(sp_richness =length(unique(species))))
+  summarise(sp.richness =length(unique(species)))
+ )
 View(richness)
 
 #visualising
-(barplot<- ggplot (richness, aes(x= site, y=sp_richness)) + geom_bar(stat="identity"))
+(barplot_richness<- ggplot (richness, aes(x= site, y=sp.richness, fill=site)) +
+    #setting the x and y axes, and asking R to give different colors to the sites
+    geom_bar(stat="identity")+
+    #specifying that we want a barplot what does stat identity do?
+    labs(x= "\nPark", y= "Species richness\n")+
+    #giving informative names to the axes
+    theme_few()+
+    #adding a theme from ggthemes - feel free to add a different one!
+    scale_fill_brewer(palette = "Accent")+ 
+    #adding a color palette - feel free to add a different one!
+    theme(legend.position = "none",
+    #removing the legend as the axes provide enough information 
+    axis.text = element_text(size = 14), 
+    axis.title = element_text(size = 16),
+    #increasing font size
+     axis.text.x = element_text(angle=45, hjust=1)))
+    #tilting the text of the x axis
+
+ggsave(barplot_richness, file="background/barplot_richness.png", width= 6, height=6)
+#save our plot - don't forget to enter your own filepath!
+
+
 
 #rank- abundance curves FAILED ATTEMPT WITH VEGAN----
 
@@ -72,15 +98,38 @@ RankAbun.1 <- rankabundance(dune)
 #accounts for variation in the number of species, AND the way individuals within a community
 #are distributed among species.
 
+#Many of the indices work with relative abundance.
+#We will add a column for that:
+
+parks_tidy<-parks_tidy %>%
+  #we don't need to make a new dataframe as we are only adding a column,
+  #not changing any of the existing ones.
+  group_by(site) %>% 
+  mutate(relative.abundance = abundance/sum(abundance)) %>% 
+  #creating a new column 
+  ungroup()
+  #removing groupings
 #Simpson's indexes
 #Describes evenness - how evenly are individuals distributed among the species that are present within a community?
 #Describes a community
 
 #Simpson's dominance
+parks_tidy %>% 
+  group_by(site) %>% 
+  summarise(simpsons.dom =sum(relative.abundance^2)) %>% 
+  ungroup()
 
 parks_tidy %>% 
   group_by(site) %>% 
-  summarise(simpsons_dominance = sum((abundance/sum(abundance))^2))
+  summarise(simpsons_dominance = sum((abundance/sum(abundance))^2)) %>% 
+  ungroup ()
+
+#shannons index
+parks_tidy %>% 
+  group_by(site) %>% 
+  summarise(shannons.div = -sum(relative.abundance*log(relative.abundance))) %>% 
+  ungroup()
+
 
 #All indices in one table
 summary<-parks_tidy %>% 
@@ -88,10 +137,21 @@ summary<-parks_tidy %>%
   summarise(simpsons_dominance = sum((abundance/sum(abundance))^2), 
             sp_richness =length(unique(species)),
             simpsons_diversity = (1-simpsons_dominance),
-            simpsons_reciprocal = (1/simpsons_dominance)) %>% 
+            simpsons_reciprocal = (1/simpsons_dominance),
+            shannons_div = -(sum((abundance/sum(abundance))*log(sum((abundance/sum(abundance))))))) %>% 
   ungroup()
 
 View(summary)
+
+#new indices in one table
+
+
+summary<- parks_tidy %>% 
+  group_by(site) %>% 
+  summarise(sp.richness = length(unique(species)),
+            shannons.diversity = -sum(relative.abundance*log(relative.abundance)),
+            simpsons.dominance = sum(relative.abundance^2)) %>% 
+  ungroup()
 
 #part 5 - SAD ----
 
@@ -113,7 +173,18 @@ parks_frequency<- parks_tidy %>%
     geom_bar(stat="identity") +
     theme_tufte()+
     facet_wrap(~ site, scale="free")+
-    labs(legen))
+labs(x= "\nNumber of individuals", y= "Number of species\n")+
+  #giving informative names to the axes
+  theme_few()+
+  #adding a theme from ggthemes - feel free to add a different one!
+  scale_fill_brewer(palette = "Accent")+ 
+  #adding a color palette - feel free to add a different one!
+  theme(legend.position = "none",
+        #removing the legend as the axes provide enough information 
+        axis.text = element_text(size = 12), 
+        axis.title = element_text(size = 16)))
+        #increasing font size
+       
 
 #what is wrong with my graph now
 
@@ -127,18 +198,25 @@ View(parks_tidy)
 
 parks_rankabundance <- parks_tidy %>% 
   group_by(site) %>% 
-  mutate(rel.abund = abundance/sum(abundance)*100,
+  mutate(rel.abund.percent = relative.abundance*100,
          rank= (rank(- abundance, ties.method="random"))) %>% 
   ungroup()
 
 (rank_abundance_plots<- parks_rankabundance %>% 
-  ggplot(aes(x=rank, y=rel.abund))+
-  geom_point(aes(color=species, fill= species))+
+  ggplot(aes(x=rank, y=rel.abund.percent))+
+  #specifying the x and y axes
+  geom_point(aes(color=species, fill= species), size=2)+
+  #increasing default point size
   geom_line(colour="black")+
-  theme_bw()+
-  labs(x="Rank", y="Relative abundance (%)")+
-  theme(legend.position="none")+
-  facet_wrap(~site, scale="free"))
+  #adding a line connecting the points
+  labs(x="\nRank", y="Relative abundance (%)\n")+
+  facet_wrap(~site, scale="free")+
+  #making separate plots for sites, allowing scales of axes to vary
+  theme_few()+
+  #adding a theme from ggthemes - feel free to add a different one!
+  theme(legend.position = "none"))
+  #removing the legend as the axes provide enough information 
+
   
   
   
